fc70464921430e5496a21fd14d301f5773aeaf76
mill <mill1983@126.com>
2017-01-05 00:59:21 +0800

    lll

--- test/fs.editor.js
+++ test/fs.editor.js
@@ -7,6 +7,7 @@ var fs = editor.create(store);
 fs.write('somefile.js', 'var a = 3;');
 var content=fs.read('somefile.js');
 console.log(content);
-fs.commit(function  () {
+//做最后的提交,才会写入到磁盘,之前的读写都是在内存中进行
+fs.commit(function  () {//commit方法第一个可选参数是过滤器,可以放gulp的插件
 	console.log(arguments.length);
 });
